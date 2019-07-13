0a6f70b9693764f28d923595f04144f8f5eab252
Richie Bendall <richiebendall@gmail.com>
2019-07-13 14:21:06 +1200

    chore: Clean up compilers and docs
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ export class DevBed {
 
     /**
     * @method
-    * @param {any} c - The client object.
+    * @param c - The client object.
     */
     constructor(c: any = client) {
         this.system = c.registerSystem(this.version.major, this.version.minor)
@@ -92,8 +92,8 @@ export class DevBed {
     /**
     * Call each callback in the array with the provided data.
     * @method
-    * @param {Function[]} arr - The array of callbacks.
-    * @param {any} data - The data to provide in the callback.
+    * @param arr - The array of callbacks.
+    * @param data - The data to provide in the callback.
     */
     private callEach(arr: Function[], data?: any): void {
         if (Array.isArray(arr)) arr.map((cb: Function) => cb(data))
@@ -102,8 +102,8 @@ export class DevBed {
     /**
     * Listen for an event.
     * @method
-    * @param {string} event - The event identifier.
-    * @param {Function} callback - The callback to trigger.
+    * @param event - The event identifier.
+    * @param callback - The callback to trigger.
     */
     public on(event: string, callback: Function): void {
         event.split(" ").map(e => {
@@ -117,8 +117,8 @@ export class DevBed {
     /**
     * Remove an event listener for an event.
     * @method
-    * @param {string} event - The event identifier.
-    * @param {Function} callback - The callback to remove.
+    * @param event - The event identifier.
+    * @param callback - The callback to remove.
     */
     public off(event: string, callback?: Function): void {
         event.split(" ").map(e => {
@@ -130,8 +130,8 @@ export class DevBed {
     /**
     * Listen for an event and trigger the callback once.
     * @method
-    * @param {string} event - The event identifier.
-    * @param {Function} callback - The callback to trigger.
+    * @param event - The event identifier.
+    * @param callback - The callback to trigger.
     */
     public once(event: string, callback: Function): void {
         const handleFire = (ev: any) => {
@@ -147,8 +147,8 @@ export class DevBed {
     /**
     * Trigger an event.
     * @method
-    * @param {string} name - The name of the event to post.
-    * @param {object} [data={}] - The data to include in the event.
+    * @param name - The name of the event to post.
+    * @param data - The data to include in the event.
     */
     public trigger(name: string, data: object = {}) {
         let eventData = this.system.createEventData(name)
@@ -160,8 +160,8 @@ export class DevBed {
     /**
     * Broadcast a custom event.
     * @method
-    * @param {string} message - The message to post.
-    * @param {string} data - The data to pass to the event handlers.
+    * @param message - The message to post.
+    * @param data - The data to pass to the event handlers.
     */
     public bc(name: string, data: object = {}): void {
         this.trigger("devbed:ev", { name, data })
@@ -173,7 +173,7 @@ export class DevBed {
     /**
     * Post a message in chat.
     * @method
-    * @param {string} message - The message to post.
+    * @param message - The message to post.
     */
     public chat(message: string): void {
         this.trigger("minecraft:display_chat_event", { message })
@@ -194,16 +194,16 @@ export class DevBed {
             log_warnings: warning
         })
     }
-    
+
     /** Core: Queries
     *   =============   */
 
     /**
     * Set a prototype value.
     * @method
-    * @param {any} val - The variable to modify.
-    * @param {string} name - The name of the prototype reference.
-    * @param {Function} func - The function to trigger when the prototype value is invoked.
+    * @param val - The variable to modify.
+    * @param name - The name of the prototype reference.
+    * @param func - The function to trigger when the prototype value is invoked.
     */
     private setProto(val: any, name: string, func: Function): any {
         let prot = Object.getPrototypeOf(val)
@@ -215,8 +215,8 @@ export class DevBed {
     /**
     * Query for an object.
     * @method
-    * @param {any} component - The component to query.
-    * @param {[any, any, any]} fields - The 3 query fields as an array of strings.
+    * @param component - The component to query.
+    * @param fields - The 3 query fields as an array of strings.
     */
     public query(component: any, fields?: [any, any, any]): any {
         let q = fields ? this.system.registerQuery(component, fields[0], fields[1], fields[2]) : this.system.registerQuery(component)
@@ -225,16 +225,16 @@ export class DevBed {
             this.system.getEntitiesFromQuery(q, cfields[0], cfields[1], cfields[2], cfields[3], cfields[4], cfields[5]) :
             this.system.getEntitiesFromQuery(q))
     }
-    
+
     /**
     * Execute a slash command.
     * @method
-    * @param {any} command - The command to execute.
-    * @param {Function} callback - The callback to invoke when the command returned data.
+    * @param command - The command to execute.
+    * @param callback - The callback to invoke when the command returned data.
     */
     public cmd(command: string, callback?: Function): void {
         if (!command.startsWith("/")) command = `/${command}`
-        this.system.executeCommand(command, ({data}: any) => {
+        this.system.executeCommand(command, ({ data }: any) => {
             if (callback) callback(data)
         })
     }
@@ -245,10 +245,10 @@ export class DevBed {
     /**
     * Get or set data.
     * @method
-    * @param {any} ref - The reference to the component.
-    * @param {any} name - The name of the component in the reference.
-    * @param {any} path - The path of the component.
-    * @param {any} value - The value to set the data to.
+    * @param ref - The reference to the component.
+    * @param name - The name of the component in the reference.
+    * @param path - The path of the component.
+    * @param value - The value to set the data to.
     */
     public data(ref: any, name: any, path?: string, value?: any): void | any {
         let data = this.system.getComponent(ref, name)
@@ -257,5 +257,4 @@ export class DevBed {
         else data = value
         this.system.applyComponentChanges(ref, data)
     }
-
 }
